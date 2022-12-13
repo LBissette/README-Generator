aa400cee2bebb7931eaaa7ddf487a30bebc75307
LBissette <loganbissette@gmail.com>
2022-12-13 14:54:03 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// TODO: Include packages needed for this application
+const fs = require('fs')
+const inquirer = require('inquirer');
+const path = "README.md"
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeFile(README, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
