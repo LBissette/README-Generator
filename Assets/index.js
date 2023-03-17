4c977ee41a7a4f77c2bb5c3fe0ec9b724f16df7a
LBissette <loganbissette@gmail.com>
2023-03-17 18:33:45 -0400

    adjusted badge placement

--- Assets/index.js
+++ Assets/index.js
@@ -9,7 +9,6 @@ const generateREADME = ({projectName, description, install, usage, tests, contri
 ## Description
 
 ${description} 
-${licenseBadge}
 
 ## Table of Contents
 
@@ -37,6 +36,8 @@ ${contributions}
 ${tests}
 
 ## License
+
+${licenseBadge}
 
 ${license}
 
@@ -138,6 +139,9 @@ inquirer.prompt(questions)
         Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
         THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.`
         answers.licenseBadge = `[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`
+    }
+    if (answers.license === 'None') {
+        answers.license = `This project does not have a license.`
     }
     const data = generateREADME(answers)
     fs.writeFile(path, data, (err) => {
